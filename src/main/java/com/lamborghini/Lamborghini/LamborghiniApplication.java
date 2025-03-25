2cf5e60ee3d06801da8756a34cc03a5e184f8289
Bianca Pulpa <biancabia41352@gmail.com>
2025-03-25 08:58:24 +0200

    Create Spring Application

---
+++ src/main/java/com/lamborghini/Lamborghini/LamborghiniApplication.java
@@ -0,0 +1,13 @@
+package com.lamborghini.Lamborghini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamborghiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamborghiniApplication.class, args);
+	}
+
+}
