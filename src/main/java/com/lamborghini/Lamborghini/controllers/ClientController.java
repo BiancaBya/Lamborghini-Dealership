1d90e5213b95cd99785840fe4fb256fbd6ab7e35
Bianca Pulpa <biancabia41352@gmail.com>
2025-03-31 20:38:13 +0300

    Create CorsConfig

--- src/main/java/com/lamborghini/Lamborghini/controllers/ClientController.java
+++ src/main/java/com/lamborghini/Lamborghini/controllers/ClientController.java
@@ -1,9 +1,14 @@
 package com.lamborghini.Lamborghini.controllers;
 
+import com.lamborghini.Lamborghini.domain.Client;
 import com.lamborghini.Lamborghini.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
@@ -17,8 +22,15 @@ public class ClientController {
     }
 
 
+    @GetMapping
+    public ResponseEntity<List<Client>> findAllClients() {
+        List<Client> clients = clientService.findAll();
+        return clients.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(clients);
+    }
 
 
 }
+
+
 
 
